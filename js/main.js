e416e34cb432d8ae747e8c2af68f646afe6810a6
MujiHDR <mujihdr@outlook.com>
2025-01-03 20:14:07 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ $(window).load(function(){
 	$('#preloader').fadeOut('slow',function(){$(this).remove();});
 });
 
+//Animator below Name i.e. MUJI HDR
+$(".rotate").textrotator({
+	animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+	separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+	speed: 2000 // How many milliseconds until the next word show.
+  });
+
 
 /******************************************************************************************************************************
 Learn More Page Scroll
